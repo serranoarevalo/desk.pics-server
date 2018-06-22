02b34f48ee8a07eed6a40bf49d440d899cae26b0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-22 21:31:54 +0300

    Boilerplate

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Options } from "graphql-yoga";
+import { createConnection } from "typeorm";
+import app from "./app";
+
+const PORT: number | string = process.env.PORT || 4000;
+const GRAPHQL_ENDPOINT: string = "/graphql";
+const SUBSCRIPTIONS_ENDPOINT: string = "/subscriptions";
+const PLAYGROUND_ENDPOINT: string = "/playground";
+
+const handleAppError = (error): void => console.log(error);
+
+const handleListening = (): void =>
+  console.log(`Listening on http://localhost:${PORT}`);
+
+const appOptions: Options = {
+  port: PORT,
+  subscriptions: SUBSCRIPTIONS_ENDPOINT,
+  playground: PLAYGROUND_ENDPOINT,
+  endpoint: GRAPHQL_ENDPOINT
+};
+
+createConnection().then(() => {
+  app.start(appOptions, handleListening);
+});
+
+app.express.on("error", handleAppError);
